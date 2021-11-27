2497fb6ec7187eb3f3cedf8b641ffa01d837a1a2
swon3210 <swon3210@gmail.com>
2021-11-28 00:14:08 +0900

    feat: 에러 & 레이아웃 관련 상수 생성

---
+++ src/constants/error.ts
@@ -0,0 +1,7 @@
+export enum ErrorText {
+  SAME_WITH_DEFAULT_CLASS_NAME = "컴포넌트의 기본 클래스 명과 동일한 클래스를 지정할 수 없습니다.",
+  WRONG_EVENT_BINDING = "이벤트 바인딩 대상이 HTMLElement 가 아닙니다.",
+  TAB_INDEX_OUT_OF_RANGE = "유효하지 않은 Tab Index 입니다",
+  DUPLICATED_COMPONENT_ID = "중복된 component id 가 존재합니다.",
+  HEADER_LINE_TEXT_SHOULD_BE_CHARACTER = "HeaderLine 의 텍스트는 한 글자여야 합니다.",
+}
---
+++ src/constants/layout.ts
@@ -0,0 +1,1 @@
+export const TABS = ["API", "로컬"];
